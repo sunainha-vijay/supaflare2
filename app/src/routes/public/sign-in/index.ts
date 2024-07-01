12615544be8ca956d3d0e5daced25c4d29d347c2
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-01 22:14:57 +0530

    Update index.ts

--- app/src/routes/public/sign-in/index.ts
+++ app/src/routes/public/sign-in/index.ts
@@ -2,21 +2,3 @@ import SignInRoute from './sign-in.vue';
 
 export { SignInRoute };
 export default SignInRoute;
-import { createRouter, createWebHistory } from 'vue-router'
-import SignUp from './sign-up.vue'
-
-const routes = [
-  // other routes
-  {
-    path: '/sign-up',
-    name: 'SignUp',
-    component: SignUp
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-})
-
-export default router
