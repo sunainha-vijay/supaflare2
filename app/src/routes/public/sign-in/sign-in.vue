aa5eb275c0203da56a6d70c053917b4130cf914c
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-02 12:56:01 +0530

    Update sign-in.vue

--- app/src/routes/public/sign-in/sign-in.vue
+++ app/src/routes/public/sign-in/sign-in.vue
@@ -1,62 +1,64 @@
-<template>
-    <public-view>
-        <div id="content">
-            <n-form ref="formRef" :model="model" :rules="rules">
-                <img class="logo" src="/supaflare.png" />
-                <h2>Welcome to our URL Shortener!</h2>
-                <h1 class="twist-url">TwistURL</h1>
-                <h1>Sign In</h1>
-                <n-form-item path="email" label="Email">
-                    <n-input v-model:value="model.email" placeholder="Enter Email" @keydown.enter="handleValidateButtonClick" />
-                </n-form-item>
-                <n-form-item path="password" label="Password">
-                    <n-input
-                        v-model:value="model.password"
-                        type="password"
-                        placeholder="Enter Password"
-                        @keydown.enter="handleValidateButtonClick"
-                    />
-                </n-form-item>
-                <div style="display: flex; justify-content: flex-end">
-                    <n-button round type="primary" @click="handleValidateButtonClick">
-                        <span v-if="model.password">Sign In</span>
-                        <span v-else>Send Magic Link</span>
-                    </n-button>
-                </div>
-                <n-divider title-placement="left">
-                    <router-link to="/signup">Don't have an account? Sign-Up</router-link>
-                </n-divider>
 
-                <n-divider title-placement="left">Or continue with</n-divider>
-                <n-space justify="space-around">
-                    <n-button @click="oauthLogin('github')">
-                        <template #icon>
-                            <n-icon>
-                                <github />
-                            </n-icon>
-                        </template>
-                        GitHub
-                    </n-button>
-                    <n-button @click="oauthLogin('google')">
-                        <template #icon>
-                            <n-icon>
-                                <google />
-                            </n-icon>
-                        </template>
-                        Google
-                    </n-button>
-                    <n-button @click="unactivatedOAuth">
-                        <template #icon>
-                            <n-icon>
-                                <facebook />
-                            </n-icon>
-                        </template>
-                        Facebook
-                    </n-button>
-                </n-space>
-            </n-form>
-        </div>
-    </public-view>
+
+<template>
+	<public-view>
+		<div id="content">
+			<n-form ref="formRef" :model="model" :rules="rules">
+				
+				<img class="logo" src="/supaflare.png" />
+				<h2>Welcome to our URL Shortener, TwistURL </h2>
+				<h1>Sign In</h1>
+				<n-form-item path="email" label="Email">
+					<n-input v-model:value="model.email" placeholder="Enter Email" @keydown.enter="handleValidateButtonClick" />
+				</n-form-item>
+				<n-form-item path="password" label="Password">
+					<n-input
+						v-model:value="model.password"
+						type="password"
+						placeholder="Enter Password"
+						@keydown.enter="handleValidateButtonClick"
+					/>
+				</n-form-item>
+				<div style="display: flex; justify-content: flex-end">
+					<n-button round type="primary" @click="handleValidateButtonClick">
+						<span v-if="model.password">Sign In</span>
+						<span v-else>Send Magic Link</span>
+					</n-button>
+				</div>
+				 <n-divider title-placement="left">
+				          <router-link to="/signup">Don't have an account? Sign-Up</router-link>
+				</n-divider>
+					
+				<n-divider title-placement="left">Or continue with</n-divider>
+				<n-space justify="space-around">
+					<n-button @click="oauthLogin('github')">
+						<template #icon>
+							<n-icon>
+								<github />
+							</n-icon>
+						</template>
+						GitHub
+					</n-button>
+					<n-button @click="oauthLogin('google')">
+						<template #icon>
+							<n-icon>
+								<google />
+							</n-icon>
+						</template>
+						Google
+					</n-button>
+					<n-button @click="unactivatedOAuth">
+						<template #icon>
+							<n-icon>
+								<Facebook />
+							</n-icon>
+						</template>
+						Facebook
+					</n-button>
+				</n-space>
+			</n-form>
+		</div>
+	</public-view>
 </template>
 
 <script lang="ts">
@@ -67,143 +69,140 @@ import { Github, Google, Facebook } from '@vicons/fa';
 import { router } from '@/router';
 
 export default defineComponent({
-    name: 'Auth',
-    components: {
-        NForm,
-        NFormItem,
-        NInput,
-        NButton,
-        NDivider,
-        NSpace,
-        NIcon,
-        Github,
-        Google,
-        Facebook,
-    },
-    setup() {
-        const messageDuration = 5000;
-        const formRef = ref();
-        const message = useMessage();
-        const modelRef = ref({
-            email: '',
-            password: '',
-        });
+	name: 'Auth',
+	components: {
+		NForm,
+		NFormItem,
+		NInput,
+		NButton,
+		NDivider,
+		NSpace,
+		NIcon,
+		Github,
+		Google,
+		Facebook,
+	},
+	setup() {
+		const messageDuration = 5000;
+		const formRef = ref();
+		const rPasswordFormItemRef = ref();
+		const message = useMessage();
+		const modelRef = ref({
+			email: '',
+			password: '',
+		});
 
-        const rules = {
-            email: [
-                {
-                    required: true,
-                    validator(rule: any, value: any) {
-                        if (!value) {
-                            return new Error('Email is required');
-                        } else if (
-                            !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
-                                value
-                            )
-                        ) {
-                            return new Error('Please enter a valid email address');
-                        }
-                        return true;
-                    },
-                    trigger: ['input', 'blur'],
-                },
-            ],
-            password: [
-                {
-                    validator(rule: any, value: any) {
-                        if (value && value.length < 6) {
-                            return new Error('Invalid Password');
-                        }
-                        return true;
-                    },
-                    trigger: ['input', 'blur'],
-                },
-            ],
-        };
+		const email = ref('');
+		const password = ref('');
 
-        async function oauthLogin(provider: string) {
-            try {
-                const { error } = await handleOAuthLogin(provider);
-                if (error) throw error;
-            } catch (error) {
-                message.error('Error authenticating with ' + provider + '...', { duration: messageDuration });
-            }
-        }
+		const rules = {
+			email: [
+				{
+					required: true,
+					validator(rule: any, value: any) {
+						if (!value) {
+							return new Error('Email is required');
+						} else if (
+							!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+								value
+							)
+						) {
+							return new Error('Please enter a valid email address');
+						}
+						return true;
+					},
+					trigger: ['input', 'blur'],
+				},
+			],
+			password: [
+				{
+					validator(rule: any, value: any) {
+						if (value && value.length < 6) {
+							return new Error('Invalid Password');
+						}
+						return true;
+					},
+					trigger: ['input', 'blur'],
+				},
+			],
+		};
 
-        function unactivatedOAuth() {
-            message.error('This OAuth method is not enabled. For display purposes only.', { duration: messageDuration });
-        }
+		async function oauthLogin(provider: any) {
+			try {
+				const { error } = await handleOAuthLogin(provider);
+				if (error) throw error;
+			} catch (error) {
+				message.error('Error authenticating with ' + provider + '...', { duration: messageDuration });
+			}
+		}
 
-        function handleValidateButtonClick(e: any) {
-            e.preventDefault();
-            if (formRef.value) {
-                formRef.value.validate(async (error: any) => {
-                    if (!error) {
-                        try {
-                            const { error, user } = await handleSignIn({
-                                email: modelRef.value.email,
-                                password: modelRef.value.password,
-                            });
-                            if (error) throw error;
-                            if (user) {
-                                router.push('/links');
-                            } else {
-                                message.success('Magic Link sent to your email!', { duration: messageDuration });
-                            }
-                        } catch (error) {
-                            message.error('Error signing in...', { duration: messageDuration });
-                        }
-                    } else {
-                        message.error('Please confirm your sign in details...', { duration: messageDuration });
-                    }
-                });
-            }
-        }
+		function unactivatedOAuth() {
+			message.error('This OAuth method is not enabled. For display purposes only.', { duration: messageDuration });
+		}
 
-        return {
-            oauthLogin,
-            unactivatedOAuth,
-            formRef,
-            model: modelRef,
-            rules,
-            handleValidateButtonClick,
-        };
-    },
+		function handleValidateButtonClick(e: any) {
+			e.preventDefault();
+			if (formRef.value) {
+				formRef.value.validate(async (error: any) => {
+					if (!error) {
+						try {
+							const { error, user } = await handleSignIn({
+								email: modelRef.value.email,
+								password: modelRef.value.password,
+							});
+							if (error) throw error;
+							if (user) {
+								router.push('/links');
+							} else {
+								message.success('Magic Link sent to your email!', { duration: messageDuration });
+							}
+						} catch (error) {
+							message.error('Error signing in...', { duration: messageDuration });
+						}
+					} else {
+						message.error('Please confirm your sign in details...', { duration: messageDuration });
+					}
+				});
+			}
+		}
+
+		return {
+			email,
+			password,
+			oauthLogin,
+			unactivatedOAuth,
+			formRef,
+			rPasswordFormItemRef,
+			model: modelRef,
+			rules,
+			handleValidateButtonClick,
+		};
+	},
 });
 </script>
 
 <style scoped>
 .logo {
-    width: 300px;
-    margin-bottom: 20px; /* Reduce the gap after the image */
+	width: 300px;
+	margin-bottom: 50px;
 }
 #content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100%;
-    text-align: center;
-}
-
-h2 {
-    margin-bottom: 5px; /* Adjust spacing between "Welcome to our URL Shortener!" and "TwistURL" */
-}
-
-.twist-url {
-    color: #4CAF50; /* Change color of TwistURL */
-    margin-bottom: 20px; /* Add space after TwistURL */
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	text-align: center;
 }
 
 form {
-    width: 400px;
+	width: 400px;
 }
 input {
-    text-align: center;
-    font-size: 20px;
+	text-align: center;
+	font-size: 20px;
 }
 h1 {
-    text-align: left;
-    margin-top: 20px; /* Add space above "Sign In" */
-    margin-bottom: 10px; /* Adjust spacing below "Sign In" */
+	text-align: left;
 }
 </style>
+
