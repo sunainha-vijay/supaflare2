38531a74e47c59328f283c8e76b642554c7c6d83
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-02 09:31:59 +0530

    Update index.ts

--- app/src/routes/public/sign-in/index.ts
+++ app/src/routes/public/sign-in/index.ts
@@ -1,12 +1,4 @@
-// import SignInRoute from './sign-in.vue';
+import SignInRoute from './sign-in.vue';
 
-// export { SignInRoute };
-// export default SignInRoute;
 export { SignInRoute };
 export default SignInRoute;
-// import SignInRoute from './sign-in.vue';
-// import SignUpRoute from './sign-up.vue';
-
-// export { SignInRoute, SignUpRoute };
-// export default SignInRoute;
-
