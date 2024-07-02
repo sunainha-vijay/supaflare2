62d4c7900822117a1ce01e5490551ca264e756af
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-02 15:46:21 +0530

    Update sign-in.vue

--- app/src/routes/public/sign-in/sign-in.vue
+++ app/src/routes/public/sign-in/sign-in.vue
@@ -203,7 +203,8 @@ export default defineComponent({
 
 form {
   width: 400px;
-  background-color: #ffffff;/* White background for form */
+  background-color: #a1c4fd;
+/* White background for form */
   border-radius: 10px; /* Rounded corners */
   padding: 20px; /* Padding inside form */
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1); /* Box shadow for depth */
