b0a3c688faedab895d1a23be0a0a2b612bbf7c83
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-04 02:17:39 +0530

    Update auth.ts

--- app/src/services/auth.ts
+++ app/src/services/auth.ts
@@ -1,61 +1,50 @@
-import { ref } from 'vue';
+
 import { supabase } from '@/services/supabase';
-import { Session, Provider, UserCredentials } from '@supabase/gotrue-js/dist/main/lib/types';
+import { Link } from '@/types/global';
+import axios, { Axios } from 'axios';
+import { useAppStore } from '@/stores/appStore';
 
-async function handleSignIn(credentials: UserCredentials) {
-	const { error, user } = await supabase.auth.signIn({
-		email: credentials.email,
-		password: credentials.password,
-	});
-	return { error, user };
-}
+const supaflareWorkerUpdateURL = import.meta.env.VITE_SUPAFLARE_WORKER_URL + '/supaflare_cfw_update';
+const appStore = useAppStore();
 
-async function handleSignUp(credentials: UserCredentials) {
-    const { email, password } = credentials;
-    try {
-        const { error, user } = await supabase.auth.signUp({ email, password });
-        if (error) {
-            throw new Error(error.message); // Throw a new Error object with the error message
-        }
-        return { error: null, user };
-    } catch (err: unknown) {
-        if (err instanceof Error) {
-            console.error('Error signing up:', err.message);
-            return { error: err.message, user: null };
-        } else {
-            console.error('Unknown error:', err);
-            return { error: 'Unknown error occurred', user: null };
-        }
-    }
-}
-
-async function handleOAuthLogin(provider: Provider) {
-	const { error } = await supabase.auth.signIn({ provider });
-	return { error };
-}
-
-async function handlePasswordReset(credentials: UserCredentials) {
-	const { email } = credentials;
-	const { error } = await supabase.auth.api.resetPasswordForEmail(String(email));
-	return { error };
-}
-
-async function handleUpdateUser(credentials: UserCredentials) {
-	const { error } = await supabase.auth.update(credentials);
-
-	return { error };
-}
-
-async function handleSignOut() {
-	const { error } = await supabase.auth.signOut();
-	return { error };
-}
-
-export {
-	handleSignIn,
-	handleOAuthLogin,
-	handleSignUp,
-	handleSignOut,
-	handlePasswordReset,
-	handleUpdateUser,
+const options = {
+	headers: {
+		'content-type': 'application/json',
+	},
 };
+
+async function fetchLinks() {
+	const { data, error } = await supabase.from('links').select('*').order('inserted_at', { ascending: false });
+	return { data, error };
+}
+
+async function addLink(link: Link) {
+	const { data, error } = await supabase.from('links').insert(link).single();
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: data.id },
+		options
+	);
+	return { data, error };
+}
+
+async function editLink(link: Link, edits: any) {
+	const { error } = await supabase.from('links').update(edits).eq('id', link.id).single();
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: link.id },
+		options
+	);
+	return { error };
+}
+
+async function deleteLink(link: Link) {
+	await supabase.from('links').delete().eq('id', link.id);
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: link.id, slug: link.slug },
+		options
+	);
+}
+
+export { fetchLinks, addLink, editLink, deleteLink };
