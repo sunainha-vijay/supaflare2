8d860479ed93e41ff517b1a5ca49916d625029db
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-04 01:09:34 +0530

    Update links.ts

--- app/src/services/links.ts
+++ app/src/services/links.ts
@@ -1,76 +1,50 @@
-// services/links.ts
 
-import axios from 'axios';
 import { supabase } from '@/services/supabase';
 import { Link } from '@/types/global';
+import axios, { Axios } from 'axios';
 import { useAppStore } from '@/stores/appStore';
 
 const supaflareWorkerUpdateURL = import.meta.env.VITE_SUPAFLARE_WORKER_URL + '/supaflare_cfw_update';
 const appStore = useAppStore();
 
 const options = {
-  headers: {
-    'Content-Type': 'application/json',
-  },
+	headers: {
+		'content-type': 'application/json',
+	},
 };
 
 async function fetchLinks() {
-  try {
-    const { data, error } = await supabase.from('links').select('*').order('inserted_at', { ascending: false });
-    if (error) {
-      throw error;
-    }
-    return data;
-  } catch (error) {
-    console.error('Error fetching links:', error.message);
-    throw error;
-  }
+	const { data, error } = await supabase.from('links').select('*').order('inserted_at', { ascending: false });
+	return { data, error };
 }
 
 async function addLink(link: Link) {
-  try {
-    const { data: insertedLink, error } = await supabase.from('links').insert(link).single();
-    if (error) {
-      throw error;
-    }
-
-    // Generate the shortened URL
-    const shortUrl = `https://supaflare2.pages.dev/${insertedLink.slug}`; // Replace with your actual domain
-
-    // Update the link with the shortened URL
-    await supabase.from('links').update({ shortUrl }).eq('id', insertedLink.id).single();
-
-    // Trigger update to Supaflare worker
-    await axios.post(supaflareWorkerUpdateURL, { token: appStore.supabaseSession!.access_token, link_id: insertedLink.id }, options);
-
-    return { data: insertedLink, shortUrl }; // Return both the inserted link and the shortened URL
-  } catch (error) {
-    console.error('Error adding link:', error.message);
-    throw error;
-  }
+	const { data, error } = await supabase.from('links').insert(link).single();
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: data.id },
+		options
+	);
+	return { data, error };
 }
 
-async function editLink(link: Link, edits: Partial<Link>) {
-  try {
-    const { error } = await supabase.from('links').update(edits).eq('id', link.id).single();
-    if (error) {
-      throw error;
-    }
-    await axios.post(supaflareWorkerUpdateURL, { token: appStore.supabaseSession!.access_token, link_id: link.id }, options);
-  } catch (error) {
-    console.error('Error editing link:', error.message);
-    throw error;
-  }
+async function editLink(link: Link, edits: any) {
+	const { error } = await supabase.from('links').update(edits).eq('id', link.id).single();
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: link.id },
+		options
+	);
+	return { error };
 }
 
 async function deleteLink(link: Link) {
-  try {
-    await supabase.from('links').delete().eq('id', link.id);
-    await axios.post(supaflareWorkerUpdateURL, { token: appStore.supabaseSession!.access_token, link_id: link.id, slug: link.slug }, options);
-  } catch (error) {
-    console.error('Error deleting link:', error.message);
-    throw error;
-  }
+	await supabase.from('links').delete().eq('id', link.id);
+	await axios.post(
+		supaflareWorkerUpdateURL,
+		{ token: appStore.supabaseSession!.access_token, link_id: link.id, slug: link.slug },
+		options
+	);
 }
 
 export { fetchLinks, addLink, editLink, deleteLink };
