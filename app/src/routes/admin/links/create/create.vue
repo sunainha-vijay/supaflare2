5becf71f126750a503a5ccfbf3fbf0a9e020d864
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-05 23:48:02 +0530

    Update create.vue

--- app/src/routes/admin/links/create/create.vue
+++ app/src/routes/admin/links/create/create.vue
@@ -34,6 +34,12 @@
 						</n-button>
 					</n-form-item>
 				</n-row>
+				<n-form-item path="start_date" label="Start Date">
+				    <n-date-picker v-model:value="model.start_date" placeholder="Select Start Date"></n-date-picker>
+				</n-form-item>
+				<n-form-item path="end_date" label="End Date">
+				    <n-date-picker v-model:value="model.end_date" placeholder="Select End Date"></n-date-picker>
+				</n-form-item>
 				<n-form-item path="android_url" label="Android URL" style="flex-grow: 1">
 					<n-input
 						v-model:value="model.android_url_raw"
@@ -80,235 +86,257 @@ import { addLink } from '@/services/links';
 import { useAppStore } from '@/stores/appStore';
 import { useLinksStore } from '@/stores/linksStore';
 import {
-	useMessage,
-	NSpin,
-	NForm,
-	NFormItem,
-	NInput,
-	NInputGroup,
-	NInputGroupLabel,
-	NIcon,
-	NButton,
-	NRow,
+    useMessage,
+    NSpin,
+    NForm,
+    NFormItem,
+    NInput,
+    NInputGroup,
+    NInputGroupLabel,
+    NIcon,
+    NButton,
+    NRow,
+    NDatePicker // Import NDatePicker from naive-ui
 } from 'naive-ui';
 import { Plus, Sync } from '@vicons/fa';
 import { customAlphabet } from 'nanoid';
 
 export default defineComponent({
-	components: { NSpin, NForm, NFormItem, NInput, NInputGroup, NInputGroupLabel, NIcon, NButton, NRow, Plus, Sync },
-	setup() {
-		const showLoadingSpinner = ref(false);
-		const formRef = ref();
-		const slugRef = ref();
-		const messageDuration = 5000;
-		const appStore = useAppStore();
-		const linksStore = useLinksStore();
-		const message = useMessage();
-		const modelRef: any = ref({
-			url: computed(() => {
-				if (!modelRef.value.url_raw[0] && !modelRef.value.url_raw[1]) return '';
-				return modelRef.value.url_raw[0] + '://' + modelRef.value.url_raw[1];
-			}),
-			url_raw: ['', ''],
-			slug: '',
-			android_url: computed(() => {
-				if (!modelRef.value.android_url_raw[0] && !modelRef.value.android_url_raw[1]) return '';
-				return modelRef.value.android_url_raw[0] + '://' + modelRef.value.android_url_raw[1];
-			}),
-			android_url_raw: ['', ''],
-			ios_url: computed(() => {
-				if (!modelRef.value.ios_url_raw[0] && !modelRef.value.ios_url_raw[1]) return '';
-				return modelRef.value.ios_url_raw[0] + '://' + modelRef.value.ios_url_raw[1];
-			}),
-			ios_url_raw: ['', ''],
-		});
+    components: { NSpin, NForm, NFormItem, NInput, NInputGroup, NInputGroupLabel, NIcon, NButton, NRow, NDatePicker, Plus, Sync },
+    setup() {
+        const showLoadingSpinner = ref(false);
+        const formRef = ref();
+        const slugRef = ref();
+        const messageDuration = 5000;
+        const appStore = useAppStore();
+        const linksStore = useLinksStore();
+        const message = useMessage();
+        const modelRef: any = ref({
+            url: computed(() => {
+                if (!modelRef.value.url_raw[0] && !modelRef.value.url_raw[1]) return '';
+                return modelRef.value.url_raw[0] + '://' + modelRef.value.url_raw[1];
+            }),
+            url_raw: ['', ''],
+            slug: '',
+            android_url: computed(() => {
+                if (!modelRef.value.android_url_raw[0] && !modelRef.value.android_url_raw[1]) return '';
+                return modelRef.value.android_url_raw[0] + '://' + modelRef.value.android_url_raw[1];
+            }),
+            android_url_raw: ['', ''],
+            ios_url: computed(() => {
+                if (!modelRef.value.ios_url_raw[0] && !modelRef.value.ios_url_raw[1]) return '';
+                return modelRef.value.ios_url_raw[0] + '://' + modelRef.value.ios_url_raw[1];
+            }),
+            ios_url_raw: ['', ''],
+            start_date: null, // Initialize start_date and end_date as null
+            end_date: null,
+        });
 
-		const rules = {
-			url: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('URL is required');
-						} else if (value.length > 2083) {
-							return new Error('URL has to be 2083 characters or below.');
-						} else if (String(value).startsWith('://')) {
-							return new Error('Please enter a protocol.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-			slug: [
-				{
-					required: true,
-					validator(rule: any, value: any) {
-						if (!value) {
-							return new Error('Slug is required');
-						}
-						if (value.length > 50) {
-							return new Error('Slug has to be 50 characters or below.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-			android_url: [
-				{
-					validator(rule: any, value: any) {
-						if (!value) {
-							return true;
-						}
-						if (value.length > 2083) {
-							return new Error('Android URL has to be 2083 characters or below.');
-						} else if (String(value).startsWith('://')) {
-							return new Error('Please enter a protocol.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-			ios_url: [
-				{
-					validator(rule: any, value: any) {
-						if (!value) {
-							return true;
-						}
-						if (value.length > 2083) {
-							return new Error('iOS URL has to be 2083 characters or below.');
-						} else if (String(value).startsWith('://')) {
-							return new Error('Please enter a protocol.');
-						}
-						return true;
-					},
-					trigger: ['input', 'blur'],
-				},
-			],
-		};
+        const rules = {
+            url: [
+                {
+                    required: true,
+                    validator(rule: any, value: any) {
+                        if (!value) {
+                            return new Error('URL is required');
+                        } else if (value.length > 2083) {
+                            return new Error('URL has to be 2083 characters or below.');
+                        } else if (String(value).startsWith('://')) {
+                            return new Error('Please enter a protocol.');
+                        }
+                        return true;
+                    },
+                    trigger: ['input', 'blur'],
+                },
+            ],
+            slug: [
+                {
+                    required: true,
+                    validator(rule: any, value: any) {
+                        if (!value) {
+                            return new Error('Slug is required');
+                        }
+                        if (value.length > 50) {
+                            return new Error('Slug has to be 50 characters or below.');
+                        }
+                        return true;
+                    },
+                    trigger: ['input', 'blur'],
+                },
+            ],
+            android_url: [
+                {
+                    validator(rule: any, value: any) {
+                        if (!value) {
+                            return true;
+                        }
+                        if (value.length > 2083) {
+                            return new Error('Android URL has to be 2083 characters or below.');
+                        } else if (String(value).startsWith('://')) {
+                            return new Error('Please enter a protocol.');
+                        }
+                        return true;
+                    },
+                    trigger: ['input', 'blur'],
+                },
+            ],
+            ios_url: [
+                {
+                    validator(rule: any, value: any) {
+                        if (!value) {
+                            return true;
+                        }
+                        if (value.length > 2083) {
+                            return new Error('iOS URL has to be 2083 characters or below.');
+                        } else if (String(value).startsWith('://')) {
+                            return new Error('Please enter a protocol.');
+                        }
+                        return true;
+                    },
+                    trigger: ['input', 'blur'],
+                },
+            ],
+            start_date: [
+                {
+                    required: true,
+                    message: 'Start Date is required',
+                    trigger: ['blur'],
+                },
+            ],
+            end_date: [
+                {
+                    required: true,
+                    message: 'End Date is required',
+                    trigger: ['blur'],
+                },
+            ],
+        };
 
-		// Remove confusion with caps I caps O and l
-		const nanoid = customAlphabet('1234567890abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ', 6);
+        // Remove confusion with caps I caps O and l
+        const nanoid = customAlphabet('1234567890abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ', 6);
 
-		async function handleGenerateSlug() {
-			modelRef.value.slug = nanoid();
-			try {
-				await slugRef.value.validate();
-			} catch (error) {
-				return;
-			}
-		}
+        async function handleGenerateSlug() {
+            modelRef.value.slug = nanoid();
+            try {
+                await slugRef.value.validate();
+            } catch (error) {
+                return;
+            }
+        }
 
-		async function handleCreateLink() {
-			try {
-				await formRef.value.validate();
-			} catch (error) {
-				return;
-			}
-			try {
-				showLoadingSpinner.value = true;
-				const { data, error } = await addLink({
-					user_id: appStore.supabaseSession!.user!.id,
-					url: modelRef.value.url,
-					slug: modelRef.value.slug,
-					meta: {
-						android_url: modelRef.value.android_url,
-						ios_url: modelRef.value.ios_url,
-					},
-				});
-				if (error) throw error;
+        async function handleCreateLink() {
+            try {
+                await formRef.value.validate();
+            } catch (error) {
+                return;
+            }
+            try {
+                showLoadingSpinner.value = true;
+                const { data, error } = await addLink({
+                    user_id: appStore.supabaseSession!.user!.id,
+                    url: modelRef.value.url,
+                    slug: modelRef.value.slug,
+                    meta: {
+                        android_url: modelRef.value.android_url,
+                        ios_url: modelRef.value.ios_url,
+                    },
+                    start_date: modelRef.value.start_date,
+                    end_date: modelRef.value.end_date,
+                });
+                if (error) throw error;
 
-				linksStore.addLink(data);
-				resetForm();
-				message.success('Link successfully created!', { duration: messageDuration });
-			} catch (error: any) {
-				if (error.code == '23505') {
-					message.error('Slug already exists. Please change the slug.', { duration: messageDuration });
-				} else {
-					message.error('Error creating link...', { duration: messageDuration });
-				}
-			} finally {
-				showLoadingSpinner.value = false;
-			}
-		}
+                linksStore.addLink(data);
+                resetForm();
+                message.success('Link successfully created!', { duration: messageDuration });
+            } catch (error: any) {
+                if (error.code == '23505') {
+                    message.error('Slug already exists. Please change the slug.', { duration: messageDuration });
+                } else {
+                    message.error('Error creating link...', { duration: messageDuration });
+                }
+            } finally {
+                showLoadingSpinner.value = false;
+            }
+        }
 
-		function resetForm() {
-			modelRef.value.url_raw = ['', ''];
-			modelRef.value.slug = '';
-			modelRef.value.android_url_raw = ['', ''];
-			modelRef.value.ios_url_raw = ['', ''];
-		}
+        function resetForm() {
+            modelRef.value.url_raw = ['', ''];
+            modelRef.value.slug = '';
+            modelRef.value.android_url_raw = ['', ''];
+            modelRef.value.ios_url_raw = ['', ''];
+            modelRef.value.start_date = null;
+            modelRef.value.end_date = null;
+        }
 
-		function handleUrlUpdate(val: any) {
-			if (String(val[0]).includes('://')) {
-				const splits = String(val[0]).split('://');
-				if (splits.length > 1) {
-					modelRef.value.url_raw[0] = splits[0];
-					modelRef.value.url_raw[1] = splits.slice(1).join('://');
-				}
-			} else if (String(val[1]).includes('://')) {
-				const splits = String(val[1]).split('://');
-				if (splits.length > 1) {
-					if (!val[0] || val[0] === splits[0]) {
-						modelRef.value.url_raw[0] = splits[0];
-						modelRef.value.url_raw[1] = splits.slice(1).join('://');
-					}
-				}
-			}
-		}
+        function handleUrlUpdate(val: any) {
+            if (String(val[0]).includes('://')) {
+                const splits = String(val[0]).split('://');
+                if (splits.length > 1) {
+                    modelRef.value.url_raw[0] = splits[0];
+                    modelRef.value.url_raw[1] = splits.slice(1).join('://');
+                }
+            } else if (String(val[1]).includes('://')) {
+                const splits = String(val[1]).split('://');
+                if (splits.length > 1) {
+                    if (!val[0] || val[0] === splits[0]) {
+                        modelRef.value.url_raw[0] = splits[0];
+                        modelRef.value.url_raw[1] = splits.slice(1).join('://');
+                    }
+                }
+            }
+        }
 
-		function handleAndroidUrlUpdate(val: any) {
-			if (String(val[0]).includes('://')) {
-				const splits = String(val[0]).split('://');
-				if (splits.length > 1) {
-					modelRef.value.android_url_raw[0] = splits[0];
-					modelRef.value.android_url_raw[1] = splits.slice(1).join('://');
-				}
-			} else if (String(val[1]).includes('://')) {
-				const splits = String(val[1]).split('://');
-				if (splits.length > 1) {
-					if (!val[0] || val[0] === splits[0]) {
-						modelRef.value.android_url_raw[0] = splits[0];
-						modelRef.value.android_url_raw[1] = splits.slice(1).join('://');
-					}
-				}
-			}
-		}
+        function handleAndroidUrlUpdate(val: any) {
+            if (String(val[0]).includes('://')) {
+                const splits = String(val[0]).split('://');
+                if (splits.length > 1) {
+                    modelRef.value.android_url_raw[0] = splits[0];
+                    modelRef.value.android_url_raw[1] = splits.slice(1).join('://');
+                }
+            } else if (String(val[1]).includes('://')) {
+                const splits = String(val[1]).split('://');
+                if (splits.length > 1) {
+                    if (!val[0] || val[0] === splits[0]) {
+                        modelRef.value.android_url_raw[0] = splits[0];
+                        modelRef.value.android_url_raw[1] = splits.slice(1).join('://');
+                    }
+                }
+            }
+        }
 
-		function handleIosUrlUpdate(val: any) {
-			if (String(val[0]).includes('://')) {
-				const splits = String(val[0]).split('://');
-				if (splits.length > 1) {
-					modelRef.value.ios_url_raw[0] = splits[0];
-					modelRef.value.ios_url_raw[1] = splits.slice(1).join('://');
-				}
-			} else if (String(val[1]).includes('://')) {
-				const splits = String(val[1]).split('://');
-				if (splits.length > 1) {
-					if (!val[0] || val[0] === splits[0]) {
-						modelRef.value.ios_url_raw[0] = splits[0];
-						modelRef.value.ios_url_raw[1] = splits.slice(1).join('://');
-					}
-				}
-			}
-		}
+        function handleIosUrlUpdate(val: any) {
+            if (String(val[0]).includes('://')) {
+                const splits = String(val[0]).split('://');
+                if (splits.length > 1) {
+                    modelRef.value.ios_url_raw[0] = splits[0];
+                    modelRef.value.ios_url_raw[1] = splits.slice(1).join('://');
+                }
+            } else if (String(val[1]).includes('://')) {
+                const splits = String(val[1]).split('://');
+                if (splits.length > 1) {
+                    if (!val[0] || val[0] === splits[0]) {
+                        modelRef.value.ios_url_raw[0] = splits[0];
+                        modelRef.value.ios_url_raw[1] = splits.slice(1).join('://');
+                    }
+                }
+            }
+        }
 
-		return {
-			showLoadingSpinner,
-			formRef,
-			slugRef,
-			model: modelRef,
-			rules,
-			handleGenerateSlug,
-			handleCreateLink,
-			handleUrlUpdate,
-			handleAndroidUrlUpdate,
-			handleIosUrlUpdate,
-		};
-	},
+        return {
+            showLoadingSpinner,
+            formRef,
+            slugRef,
+            model: modelRef,
+            rules,
+            handleGenerateSlug,
+            handleCreateLink,
+            handleUrlUpdate,
+            handleAndroidUrlUpdate,
+            handleIosUrlUpdate,
+        };
+    },
 });
+
 </script>
 
 <style scoped>
