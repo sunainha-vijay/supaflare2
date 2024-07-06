958d59c1b070b2b90c89b6cd2c4a6070dfae9b8c
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-06 22:11:59 +0530

    Update router.ts

--- app/src/router.ts
+++ app/src/router.ts
@@ -1,9 +1,7 @@
-// router.ts
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import { useAppStore } from '@/stores/appStore';
 import { supabase } from '@/services/supabase';
-import { fetchLinkBySlug } from '@/services/links';
-
+import SignUpRoute from '@/routes/public/sign-up';
 import SignInRoute from '@/routes/public/sign-in';
 
 const NotFoundRoute = () => import('@/routes/admin/not-found');
@@ -17,114 +15,108 @@ const defaultTitle = 'Supaflare';
 let appStore: any;
 
 export const defaultRoutes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Sign In',
-    component: SignInRoute,
-    meta: {
-      requiresAuth: false,
-    },
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardRoute,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: '/links',
-    name: 'Manage Links',
-    component: ManageLinkRoute,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: '/link/create',
-    name: 'Create Link',
-    component: CreateLinkRoute,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: SettingsRoute,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: '/:_(.+)+',
-    name: 'Page Not Found',
-    component: NotFoundRoute,
-    meta: {
-      requiresAuth: true,
-    },
-  },
+	{
+		path: '/',
+		name: 'Sign In',
+		component: SignInRoute,
+		meta: {
+			requiresAuth: false,
+		},
+	},
+	{
+		path: '/signup',
+		name: 'Sign Up',
+		component: SignUpRoute,
+		meta: {
+		       requiresAuth: false, // Adjust as needed
+		},
+	},	
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		component: DashboardRoute,
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/links',
+		name: 'Manage Links',
+		component: ManageLinkRoute,
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/link/create',
+		name: 'Create Link',
+		component: CreateLinkRoute,
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/settings',
+		name: 'Settings',
+		component: SettingsRoute,
+		meta: {
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/:_(.+)+',
+		name: 'Page Not Found',
+		component: NotFoundRoute,
+		meta: {
+			requiresAuth: true,
+		},
+	},
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
-  routes: defaultRoutes,
+	history: createWebHistory(),
+	routes: defaultRoutes,
 });
 
 router.beforeEach(async (to, from, next) => {
-  if (!appStore) {
-    appStore = useAppStore();
-  }
+	if (!appStore) {
+		appStore = useAppStore();
+	}
 
-  if (!from.name) {
-    appStore.initialPath = to.path;
-  }
+	if (!from.name) {
+		appStore.initialPath = to.path;
+	}
 
-  const currentDate = new Date();
+	if (to.path === '/' && appStore.supabaseSession) {
+		next({ path: '/links' });
+		return;
+	}
 
-  if (to.path !== '/' && appStore.supabaseSession) {
-    const { data: link, error } = await fetchLinkBySlug(to.path.substring(1)); // Fetch the link by slug
-    if (link) {
-      const startDate = new Date(link.start_date);
-      const endDate = new Date(link.end_date);
-      if (currentDate < startDate || currentDate > endDate) {
-        next({ path: '/not-found' });
-        return;
-      }
-    }
-  }
-
-  if (to.path === '/' && appStore.supabaseSession) {
-    next({ path: '/links' });
-    return;
-  }
-
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!appStore.supabaseSession) {
-      next({ path: '/' });
-    } else {
-      next();
-    }
-  } else {
-    next();
-  }
+	if (to.matched.some((record) => record.meta.requiresAuth)) {
+		if (!appStore.supabaseSession) {
+			next({ path: '/' });
+		} else {
+			next();
+		}
+	} else {
+		next();
+	}
 });
 
 router.afterEach((to) => {
-  document.title = to.name ? String(to.name) : defaultTitle;
+	document.title = to.name ? String(to.name) : defaultTitle;
 });
 
 supabase.auth.onAuthStateChange((event, session) => {
-  if (!appStore) {
-    appStore = useAppStore();
-  }
+	if (!appStore) {
+		appStore = useAppStore();
+	}
 
-  appStore.supabaseSession = session;
+	appStore.supabaseSession = session;
 
-  if (appStore.initialPath) {
-    const initialPath = appStore.initialPath;
-    appStore.initialPath = '';
-    router.push(initialPath);
-  }
+	if (appStore.initialPath) {
+		const initialPath = appStore.initialPath;
+		appStore.initialPath = '';
+		router.push(initialPath);
+	}
 });
