6352143a22571fb036b6a7eb5a6070d36cb62eaf
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-06 22:09:01 +0530

    Update global.ts

--- app/src/types/global.ts
+++ app/src/types/global.ts
@@ -1,15 +1,12 @@
-// src/types/global.d.ts or wherever your types are defined
 export declare interface Link {
-    id?: string;
-    user_id: string;
-    url: string;
-    slug: string;
-    meta?: {
-        android_url?: string;
-        ios_url?: string;
-    };
-    updated_at?: string;
-    inserted_at?: string;
-    start_date?: string; // New property for start date
-    end_date?: string; // New property for end date
+	id?: string;
+	user_id: string;
+	url: string;
+	slug: string;
+	meta?: {
+		android_url?: string,
+		ios_url?: string,
+	};
+	updated_at?: string;
+	inserted_at?: string;
 }
