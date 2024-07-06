91925a567a485dd709eb040795425c61d02a6c91
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-06 18:53:41 +0530

    Update create.vue

--- app/src/routes/admin/links/create/create.vue
+++ app/src/routes/admin/links/create/create.vue
@@ -1,3 +1,94 @@
+<template>
+	<admin-view>
+		<h1>Create Link</h1>
+		<n-spin :show="showLoadingSpinner">
+			<n-form ref="formRef" class="centered-form" :model="model" :rules="rules">
+				<n-form-item path="url" label="URL">
+					<n-input
+						v-model:value="model.url_raw"
+						class="url-input"
+						pair
+						clearable
+						separator="://"
+						:placeholder="['Protocol', 'Web Address']"
+						@change="handleUrlUpdate"
+						@update:value="handleUrlUpdate"
+					></n-input>
+				</n-form-item>
+				<n-row>
+					<n-form-item ref="slugRef" path="slug" label="Slug" style="flex-grow: 1">
+						<n-input-group>
+							<n-input-group-label class="slug-input-inline">/</n-input-group-label>
+							<n-input v-model:value="model.slug" class="slug-input" placeholder="Enter Slug" />
+						</n-input-group>
+					</n-form-item>
+					<n-form-item>
+						<n-button type="warning" style="margin-left: 20px" @click="handleGenerateSlug">
+							<template #icon>
+								<n-icon>
+									<sync />
+								</n-icon>
+							</template>
+							Generate Slug
+						</n-button>
+					</n-form-item>
+				</n-row>
+        <n-form-item path="start_date" label="Start Date">
+          <n-date-picker
+            v-model:value="model.start_date"
+            type="datetime"
+            placeholder="Select Start Date"
+          />
+        </n-form-item>
+        <n-form-item path="end_date" label="End Date">
+          <n-date-picker
+            v-model:value="model.end_date"
+            type="datetime"
+            placeholder="Select End Date"
+          />
+        </n-form-item>
+				<n-form-item path="android_url" label="Android URL" style="flex-grow: 1">
+					<n-input
+						v-model:value="model.android_url_raw"
+						class="url-input"
+						pair
+						clearable
+						separator="://"
+						:placeholder="['Protocol', 'Web Address']"
+						@change="handleAndroidUrlUpdate"
+						@update:value="handleAndroidUrlUpdate"
+					></n-input>
+				</n-form-item>
+				<n-form-item path="ios_url" label="iOS URL" style="flex-grow: 1">
+					<n-input
+						v-model:value="model.ios_url_raw"
+						class="url-input"
+						pair
+						clearable
+						separator="://"
+						:placeholder="['Protocol', 'Web Address']"
+						@change="handleIosUrlUpdate"
+						@update:value="handleIosUrlUpdate"
+					></n-input>
+				</n-form-item>
+
+				<div style="display: flex; justify-content: center">
+					<n-button round type="primary" :disabled="showLoadingSpinner" @click="handleCreateLink">
+						<template #icon>
+							<n-icon>
+								<plus />
+							</n-icon>
+						</template>
+						Create
+					</n-button>
+				</div>
+			</n-form>
+		</n-spin>
+	</admin-view>
+</template>
+
+<script lang="ts">
+
 import { defineComponent, ref, computed } from 'vue';
 import { addLink } from '@/services/links';
 import { useAppStore } from '@/stores/appStore';
@@ -33,6 +124,8 @@ export default defineComponent({
         return modelRef.value.ios_url_raw[0] + '://' + modelRef.value.ios_url_raw[1];
       }),
       ios_url_raw: ['', ''],
+      start_date: '',
+      end_date: '',
     });
 
     const rules = {
@@ -221,3 +314,33 @@ export default defineComponent({
     };
   },
 });
+</script>
+
+<style scoped>
+.centered-form {
+	width: 500px;
+	margin: 0 auto;
+}
+
+.slug-input {
+	text-align: center;
+}
+
+.slug-input-inline {
+	width: '33%';
+	text-align: 'right';
+}
+
+.url-input :deep(.n-input-wrapper):first-child {
+	flex-grow: 0;
+	width: 80px;
+}
+
+.url-input :deep(.n-input-wrapper):first-child input {
+	text-align: right;
+}
+
+.url-input :deep(.n-input-wrapper):nth-child(3) input {
+	text-align: left;
+}
+</style>
