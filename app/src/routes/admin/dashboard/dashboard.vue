03abb8e85043eb332d141919a7ff02624d484e35
Sunainha Vijay <113001688+sunainha-vijay@users.noreply.github.com>
2024-07-06 22:04:45 +0530

    Update dashboard.vue

--- app/src/routes/admin/dashboard/dashboard.vue
+++ app/src/routes/admin/dashboard/dashboard.vue
@@ -1,39 +1,16 @@
 <template>
-  <admin-view>
-    <h1>Dashboard</h1>
-    <n-table :columns="columns" :data="links">
-      <template #cell-start_date="{ row }">
-        {{ new Date(row.start_date).toLocaleString() }}
-      </template>
-      <template #cell-end_date="{ row }">
-        {{ new Date(row.end_date).toLocaleString() }}
-      </template>
-    </n-table>
-  </admin-view>
+	<admin-view>
+		<h1>Dashboard</h1>
+	</admin-view>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from 'vue';
-import { fetchLinks } from '@/services/links';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  components: {},
-  setup() {
-    const links = ref<any[]>([]); // Initialize with an empty array
-    const columns = ref([
-      { title: 'Short URL', key: 'slug' },
-      { title: 'Long URL', key: 'url' },
-      { title: 'Start Date', key: 'start_date' },
-      { title: 'End Date', key: 'end_date' },
-      // Additional columns...
-    ]);
-
-    onMounted(async () => {
-      const { data } = await fetchLinks();
-      links.value = data || [];
-    });
-
-    return { links, columns };
-  },
+	components: {},
+	setup() {
+		return {};
+	},
 });
 </script>
